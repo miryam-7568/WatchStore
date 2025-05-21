47ad892716658ba7db93f2bdd6ccad5efe0b66df
Miriam Rabin <327742698@PC5-17>
2025-05-21 13:31:51 +0300

    start adding tests

---
+++ WatchStore/TestProject/CategoryRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace TestProject
+{
+    public class CategoryRepositoryTest
+    {
+        [Fact]
+        public async Task GetCategories_returnsCategories()
+        {
+            var category1= new Category { Id = 1, CategoryName = "a" };
+            var category2 = new Category { Id = 2, CategoryName = "b" };
+            var category3 = new Category { Id = 3, CategoryName = "c" };
+            var categories = new List<Category>() {category1, category2, category3 };
+
+        }
+    }
+}
